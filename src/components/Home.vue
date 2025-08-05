1366b651d7b9202b84b66027c72160ed5ec7789d
wenjunhao <wenjunhao@gurobot.com>
2025-08-05 18:21:04 +0800

    feat(Home):添加首页图

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,24 @@
 <script setup></script>
 <template>
-    <div>
-        123
+    <div class="top_nav"></div>
+    <div class="first_img">
+        <img src="/public/Home_bg.jpg"/>
     </div>
 </template>
+<style scoped>
+.top_nav{
+    width: 100%;
+    height: 47%;
+}
+.first_img{
+    width: 1847px;
+    height: 791px;
+    margin: 0 auto;
+}
+img{
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+}
+
+</style>
