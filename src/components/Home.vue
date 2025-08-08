a378d7570e464b43c04bad49743dbabc36eafa78
wenjunhao <wenjunhao@gurobot.com>
2025-08-08 09:32:22 +0800

    feat(Home):调整首页图片样式和尺寸

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -11,14 +11,17 @@
     height: 47%;
 }
 .first_img{
-    width: 1847px;
-    height: 791px;
+    width: 1800px;
+    height: 950px;
     margin: 0 auto;
+    text-align: center;
+    border-radius: 20px;
+    box-shadow: 4px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    overflow: hidden;
 }
 img{
     width: 100%;
     height: 100%;
-    object-fit: contain;
+    display: inline-block;
 }
-
 </style>
