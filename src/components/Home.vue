72c4bbc7dd3acb4da44fa75e30aed707e50765be
wenjunhao <wenjunhao@gurobot.com>
2025-08-14 10:53:25 +0800

    fix(TopNav):修复首页头部栏组件化

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,25 +5,14 @@ import HomeImage from './HomeImage.vue';
 </script>
 <template>
     <div class="first_screen">
-        <div class="top_nav">
-            <TopNav />
-        </div>
+        <TopNav />
         <HomeImage />
-        <div class="navigation">
-            <Navigation />
-        </div>
+        <Navigation />
     </div>
 </template>
 <style scoped>
 .first_screen {
     width: 100vw;
     height: 100vh;
-}
-
-.top_nav {
-    width: 100vw;
-    height: 61px;
-    /* 可以添加导航栏背景色以便在各种背景下可见 */
-    /* background-color: rgba(0, 0, 0, 0.5); */
 }
 </style>
--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,18 +1,27 @@
 <template>
-    <div class="navigation-content">
-        <div class="top_nav_left">
-            SZTUFA
-        </div>
-        <div class="top_nav_middle">
-            因为热爱 一往无前
-        </div>
-        <div class="top_nav_right">
-            联系我们
+    <div class="top_nav">
+        <div class="navigation-content">
+            <div class="top_nav_left">
+                SZTUFA
+            </div>
+            <div class="top_nav_middle">
+                因为热爱 一往无前
+            </div>
+            <div class="top_nav_right">
+                联系我们
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped>
+.top_nav {
+    width: 100vw;
+    height: 61px;
+    /* 可以添加导航栏背景色以便在各种背景下可见 */
+    /* background-color: rgba(0, 0, 0, 0.5); */
+}
+
 .navigation-content {
     display: flex;
     justify-content: space-between;
