03e1b0fcf3397eefefd2aa1c0b020e371c7a9749
wenjunhao <wenjunhao@gurobot.com>
2025-08-14 14:55:07 +0800

    feat(Home):添加首页第二屏

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -22,7 +22,7 @@ const ImageSrc = [
             <div class="white_block">
                 <h1 class="title">推文精选</h1>
             </div>
-            <ImageScroll :images="ImageSrc" :interval="3000" :imageWidth="300" />
+            <ImageScroll class="imageScroll" :images="ImageSrc" :interval="3000" :imageWidth="300" />
         </div>
     </div>
 </template>
@@ -35,5 +35,48 @@ const ImageSrc = [
 .second_screen {
     width: 100%;
     height: 100vh;
+}
+
+.second_screen_content {
+    position: relative;
+}
+
+.white_block {
+    margin: 0 auto;
+    margin-top: 134px;
+    width: 70%;
+    height: 812px;
+    border-radius: 20px;
+    background: rgba(255, 255, 255, 1);
+    overflow: visible;
+    position: relative;
+}
+
+.title {
+    position: absolute;
+    top: 10px;
+    left: 50%;
+    transform: translateX(-50%);
+
+    font-family: 'YouSheBiaoTiHei';
+    color: rgba(78, 99, 142, 1);
+    font-size: 170px;
+    font-weight: 400;
+    line-height: 221px;
+    letter-spacing: -2px;
+    text-align: left;
+}
+
+.imageScroll {
+    position: absolute;
+    top: 200px;
+    left: 50%;
+    transform: translateX(-50%);
+}
+
+@font-face {
+    font-family: 'YouSheBiaoTiHei';
+    src: url('..//assets/YouSheBiaoTiHei.ttf');
+    font-display: swap;
 }
 </style>
